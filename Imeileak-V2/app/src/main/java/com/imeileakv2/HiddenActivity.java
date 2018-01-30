aec3ef7aeae5d5531daa4113d7e1e6a754aad42c
shivapbhusal <civabhusal@gmail.com>
2018-01-30 13:51:30 -0500

    refactor, clean up password app

--- Imeileak-V2/app/src/main/java/com/imeileakv2/HiddenActivity.java
+++ Imeileak-V2/app/src/main/java/com/imeileakv2/HiddenActivity.java
@@ -21,7 +21,6 @@ public class HiddenActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
-
         Button b2 = (Button)findViewById(R.id.button2);
         b2.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
