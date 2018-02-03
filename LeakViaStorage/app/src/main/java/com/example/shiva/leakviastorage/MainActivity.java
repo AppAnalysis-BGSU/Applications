eadbc2ae4d011283fa4942e1a0bbf7f0123f49f4
shivapbhusal <civabhusal@gmail.com>
2018-02-03 15:31:27 -0500

    add skeleton to MainActivity

--- LeakViaStorage/app/src/main/java/com/example/shiva/leakviastorage/MainActivity.java
+++ LeakViaStorage/app/src/main/java/com/example/shiva/leakviastorage/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.shiva.leakviastorage;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.telephony.TelephonyManager;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,31 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        String imei=getImei();
+        uploadImeiToDb(imei);
     }
+
+    public String getImei()
+    {
+        TelephonyManager telephonyManager = (TelephonyManager) getSystemService(Context.TELEPHONY_SERVICE);
+        String imei = telephonyManager.getDeviceId(); // Source
+        return imei;
+    }
+
+    public void uploadImeiToDb(String imei)
+    {
+
+    }
+
+    public String retriveImei()
+    {
+        String str="";
+        return str;
+    }
+
+    public void leakImei()
+    {
+
+    }
+    
 }
