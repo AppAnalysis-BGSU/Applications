d93f2aabb61e6481ca17abfd16d2394381fb2ce8
shivapbhusal <civabhusal@gmail.com>
2018-02-03 15:21:21 -0500

    add Databasehelper class

---
+++ LeakViaStorage/app/src/main/java/com/example/shiva/leakviastorage/Databasehelper.java
@@ -0,0 +1,30 @@
+package com.example.shiva.leakviastorage;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by Shiva on 2/3/2018.
+ */
+
+public class Databasehelper extends SQLiteOpenHelper {
+    public static final String DATABASE_NAME="Info.db";
+    public static final String TABLE_NAME="imei_table";
+    public static final String COL1="IMEI";
+
+    public Databasehelper(Context context) {
+        super(context, DATABASE_NAME, null, 1);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL("CREATE TABLE "+TABLE_NAME+"( IMEI TEXT)");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS"+TABLE_NAME);
+        onCreate(db);
+    }
+}
