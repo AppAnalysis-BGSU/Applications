c0a88591fe06fe9a77c81ea6218156362df627b9
shivapbhusal <civabhusal@gmail.com>
2018-02-11 17:09:32 -0500

    add data entry to sharedpreference

--- SharedPreference/app/src/main/java/com/example/shiva/sharedpreference/MainActivity.java
+++ SharedPreference/app/src/main/java/com/example/shiva/sharedpreference/MainActivity.java
@@ -1,13 +1,22 @@
 package com.example.shiva.sharedpreference;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    public static final String PREFS_NAME="MyPrefsFile";
+    public static final String IMEI="imei";
+    SharedPreferences sharedPref;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        sharedPref=getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor=sharedPref.edit();
+        editor.putString(IMEI,"00000");
+        editor.commit();
     }
 }
